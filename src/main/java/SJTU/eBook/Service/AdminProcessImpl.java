c6ea7e99874eae02d78f8c81d1fb5f0693c6a161
kmykoh97 <kmykoh97@gmail.com>
2019-05-23 19:24:42 +0800

    refactor code

--- src/main/java/SJTU/eBook/Service/AdminProcessImpl.java
+++ src/main/java/SJTU/eBook/Service/AdminProcessImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
-
 import java.util.List;
 
 @RestController
